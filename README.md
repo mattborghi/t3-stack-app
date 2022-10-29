695531b70a8b82e63ae71912ef8dcd87aa3bb094
Matias Borghi <borghi.matias@gmail.com>
2022-10-29 19:08:47 -0300

    add tRPC with random pokemon fetcher

--- README.md
+++ README.md
@@ -32,3 +32,28 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## File structure
+
+```
+.
+├── prisma  # <-- if prisma is added
+│   └── [..]
+├── src
+│   ├── pages
+│   │   ├── _app.tsx  # <-- add `withTRPC()`-HOC here
+│   │   ├── api
+│   │   │   └── trpc
+│   │   │       └── [trpc].ts  # <-- tRPC HTTP handler
+│   │   └── [..]
+│   ├── server
+│   │   ├── routers
+│   │   │   ├── _app.ts  # <-- main app router
+│   │   │   ├── post.ts  # <-- sub routers
+│   │   │   └── [..]
+│   │   ├── context.ts   # <-- create app context
+│   │   └── trpc.ts      # <-- procedure helpers
+│   └── utils
+│       └── trpc.ts  # <-- your typesafe tRPC hooks
+└── [..]
+```
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { trpc } from "@/utils/trpc";
+import { useEffect, useState } from "react";
+
+type Pair = {
+  first: number;
+  second: number;
+};
+
 export default function Home() {
+  const [initialValues, setInitialValues] = useState<Pair>();
+
+  useEffect(() => {
+    const [first, second] = getOptionsForVote();
+    setInitialValues({ first, second });
+  }, []);
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
-      <div className="p-2"/>
+      <div className="p-2" />
       <div className="border rounded p-8 flex justify-evenly items-center max-w-2-xl">
-        <div className="w-16 h-16 bg-red-200" />
-        <div className="p-8" >vs</div>
-        <div className="w-16 h-16 bg-red-200" />
-        </div>
+        <div className="w-16 h-16 bg-red-800">{initialValues?.first}</div>
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-800">{initialValues?.second}</div>
+      </div>
     </div>
-  )
+  );
 }
