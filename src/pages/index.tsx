c774962051597fd58ebae4efa43d55ddafc3b225
Matias Borghi <borghi.matias@gmail.com>
2022-10-29 20:32:09 -0300

    adding buttons and additional pokemon info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 import { useEffect, useState } from "react";
 
+const ButtonClasses =
+  "border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline";
+
 type Pair = {
   first: number;
   second: number;
@@ -24,27 +27,48 @@ export default function Home() {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForRoundest = (selection: number | undefined) => {
+    if (!selection) return null;
+    console.log("voting for " + selection);
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-evenly items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="flex flex-col items-center">
           <img
             alt={firstPokemon.data?.name}
             src={firstPokemon.data?.sprites.front_default ?? ""}
-            className="w-full"
+            className="w-64 h-64"
           />
-          <div className="text-xl text-center capitalize mt-[-1rem]">{firstPokemon.data?.name}</div>
+          <div className="text-xl text-center capitalize mt-[-1rem]">
+            {firstPokemon.data?.name}
+          </div>
+          <button
+            className={ButtonClasses}
+            onClick={() => voteForRoundest(initialValues?.first)}
+          >
+            Rounder
+          </button>
         </div>
         <div className="p-8">vs</div>
-        <div className="w-64 h-64 flex flex-col">
+        <div className="flex flex-col items-center">
           <img
             alt={secondPokemon.data?.name}
             src={secondPokemon.data?.sprites.front_default ?? ""}
-            className="w-full"
+            className="w-64 h-64"
           />
-          <div className="text-xl text-center capitalize mt-[-1rem]">{secondPokemon.data?.name}</div>
+          <div className="text-xl text-center capitalize mt-[-1rem]">
+            {secondPokemon.data?.name}
+          </div>
+          <button
+            className={ButtonClasses}
+            onClick={() => voteForRoundest(initialValues?.second)}
+          >
+            Rounder
+          </button>
         </div>
         <div className="p-2" />
       </div>
