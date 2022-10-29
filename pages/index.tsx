8a5011afd2134f84d5baf32aad85f120edce3bce
Matias Borghi <borghi.matias@gmail.com>
2022-10-29 17:22:19 -0300

    basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1 className='text-3xl bold'>Hello world!</h1>
+    <div className="h-screen w-screen flex flex-col justify-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-evenly items-center max-w-2-xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8" >vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+        </div>
+    </div>
   )
 }
