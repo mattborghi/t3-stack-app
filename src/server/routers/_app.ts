8f1db0e1d436095537afb08f6be94f9fa21d96b1
Matias Borghi <borghi.matias@gmail.com>
2022-10-29 20:09:20 -0300

    send less backend data

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -14,7 +14,7 @@ export const appRouter = router({
       const api = new PokemonClient();
 
       const pokemon = await api.getPokemonById(input.id);
-      return pokemon;
+      return {name: pokemon.name, sprites: pokemon.sprites};
     }),
 });
 
